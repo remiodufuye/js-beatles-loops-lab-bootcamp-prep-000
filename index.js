240e4a9567d68da38b710a50deb83ca762d1d8d8
remi.odufuye <remi.odufuye@gmail.com>
2019-10-03 14:19:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,32 @@
 //  musicians is the musicians array 
 // instruments is the instruments array 
 
-var emptyArray = [] ;
 
-function theBeatlesPlay(musicians , instruments) {
-  
+function theBeatlesPlay(musicians, instruments) {
+  const array = [];
+
+  for(let i =0, l = musicians.length; i < l; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  const loudFacts = [];
+  let i = 0;
+  while(i<facts.length){
+    loudFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+  return loudFacts;
+}
+
+function iLoveTheBeatles(n) {
+  const array = [];
+
+  do {
+    array.push("I love the Beatles!")
+    n++
+  } while(n<15);
+  return array;
 }
